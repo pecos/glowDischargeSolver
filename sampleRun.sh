7af4d5bf0c21e1fa873b68057ec13b2c0b2accc9
Todd A. Oliver <oliver@oden.utexas.edu>
2021-02-17 10:36:05 -0600

    Update sample run and plotting for new case

--- sampleRun.sh
+++ sampleRun.sh
@@ -7,15 +7,24 @@ error_exit()
 }
 
 EXE="python3 ./chebSolver.py"
-Np=150
-Nt=6400
-dt=0.015625
+#Np=150
+#Nt=6400
+#dt=0.015625
+Np=250
+Nt=2560
+dt=0.00390625
 
 baseCmd="$EXE --Np $Np --Nt $Nt --dt $dt"
 screenOut="run.out"
 rm -f $screenOut
 
 # Run an example case of the Chebyshev time domain solver
-$baseCmd --t0   0.0 --outfile restart_Np150_T100.npy  > run.out || error_exit "First run failed"
-$baseCmd --t0 100.0 --restart restart_Np150_T100.npy \
-	            --outfile restart_Np150_T200.npy >> run.out || error_exit "Second run failed"
+$baseCmd --t0   0.0 --outfile restart_psaap_Np250_T10.npy  > run.out || error_exit "First run failed"
+$baseCmd --t0  10.0 --restart restart_psaap_Np250_T10.npy \
+                    --outfile restart_psaap_Np250_T20.npy >> run.out || error_exit "Second run failed"
+$baseCmd --t0  20.0 --restart restart_psaap_Np250_T20.npy \
+                    --outfile restart_psaap_Np250_T30.npy >> run.out || error_exit "Third run failed"
+$baseCmd --t0  30.0 --restart restart_psaap_Np250_T30.npy \
+                    --outfile restart_psaap_Np250_T40.npy >> run.out || error_exit "Fourth run failed"
+$baseCmd --t0  40.0 --restart restart_psaap_Np250_T40.npy \
+                    --outfile restart_psaap_Np250_T50.npy >> run.out || error_exit "Fifth run failed"
