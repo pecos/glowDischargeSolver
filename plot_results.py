76255cc376aa5a74a7334db0dd5b77502626231b
Juan Pablo Barberena Valencia <jbarbere@quartz764.llnl.gov>
2022-09-15 11:39:49 -0700

    Added exporting data to CSV file

--- plot_results.py
+++ plot_results.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import chebSolver as cs
+import csv
+import sys
+#import chebSolver as cs
 import numpy.polynomial.chebyshev as cheb
 import matplotlib
+import matplotlib.font_manager
 from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Times']})
 rc('text', usetex=True)
+
+sys.path.insert(0, '../')
+import chebSolver as cs
 
 # these values are required to "redimensionalize" the results
 # they must be consistent with the scenario input file
@@ -18,7 +24,7 @@ V0 = 100.0
 Np=150
 
 # load solution file
-D = np.load('newton_4spec_Np150_fullsoln_AllSamples.npy')
+D = np.load('newton_4spec_CN_Np150_fullsoln.npy')
 D = np.transpose(D)
 
 # T = number of time steps
@@ -52,12 +58,12 @@ Te = D[4*Np:,:]/ne
 
 # evaluate electric potential (this requires a poisson solve, which is
 # why the timeDomainCollocationSolver is loaded)
-phi = np.zeros((Np,T+1))
-tds = cs.timeDomainCollocationSolver(4,1,Np,scenario=2)
-print("Solving poisson...")
-for i in range(0,T+1):
-    tds.solve_poisson(ne[:,i],ni[:,i],tp[i])
-    phi[:,i] = tds.phi[:]
+#phi = np.zeros((Np,T+1))
+#tds = cs.timeDomainCollocationSolver(4,1,Np,scenario=6)
+#print("Solving poisson...")
+#for i in range(0,T+1):
+#    tds.solve_poisson(ne[:,i],ni[:,i],tp[i])
+#    phi[:,i] = tds.phi[:]
 
 # make some plots
 
@@ -118,7 +124,7 @@ fig,ax = plt.subplots()
 h = ax.contourf(xr, tr, np.transpose(nb)*ne0, levels=np.linspace(0,5*ne0,129))
 cbar = plt.colorbar(h, ticks=np.linspace(0,ne0,9))
 cbar.ax.set_ylabel(r"$n_b$ [m$^{-3}$]", labelpad=6,fontsize=18)
-plt.setp(cbar.ax.set_yticklabels(), rotation='horizontal', fontsize=12)
+plt.setp(cbar.ax.get_yticklabels(), rotation='horizontal', fontsize=12)
 ax.contour(xr, tr, np.transpose(nb)*ne0, levels=np.linspace(0,ne0,129))
 ax.set_xlabel(r"$x$ [m]", fontsize=18)
 plt.setp(ax.get_xticklabels(), fontsize=12)
@@ -127,17 +133,17 @@ plt.setp(ax.get_yticklabels(), fontsize=12)
 plt.savefig('nb_4spec_contour.pdf')
 
 # phi
-fig,ax = plt.subplots()
-h = ax.contourf(xr, tr, np.transpose(phi)*V0, levels=np.linspace(-120,120,129))
-cbar = plt.colorbar(h, ticks=np.linspace(-120,120,13))
-cbar.ax.set_ylabel(r"$\phi$ [V]", labelpad=6, fontsize=18)
-plt.setp(cbar.ax.get_yticklabels(), rotation='horizontal', fontsize=12)
-ax.contour(xr, tr, np.transpose(phi)*V0, levels=np.linspace(-120,120,129))
-ax.set_xlabel(r"$x$ [m]", fontsize=18)
-plt.setp(ax.get_xticklabels(), fontsize=12)
-ax.set_ylabel(r"$t$ [s]", fontsize=18)
-plt.setp(ax.get_yticklabels(), fontsize=12)
-plt.savefig('phi_4spec_contour.pdf')
+#fig,ax = plt.subplots()
+#h = ax.contourf(xr, tr, np.transpose(phi)*V0, levels=np.linspace(-120,120,129))
+#cbar = plt.colorbar(h, ticks=np.linspace(-120,120,13))
+#cbar.ax.set_ylabel(r"$\phi$ [V]", labelpad=6, fontsize=18)
+#plt.setp(cbar.ax.get_yticklabels(), rotation='horizontal', fontsize=12)
+#ax.contour(xr, tr, np.transpose(phi)*V0, levels=np.linspace(-120,120,129))
+#ax.set_xlabel(r"$x$ [m]", fontsize=18)
+#plt.setp(ax.get_xticklabels(), fontsize=12)
+#ax.set_ylabel(r"$t$ [s]", fontsize=18)
+#plt.setp(ax.get_yticklabels(), fontsize=12)
+#plt.savefig('phi_4spec_contour.pdf')
 
 print("Plotting lines...")
 
@@ -173,19 +179,19 @@ ax.plot(xr, (2./3.)*Te[:,t3], 'c-', lw=2, label=r"t={0:.2f}T".format(tp[t3]))
 ax.set_xlim((0,0.0254))
 ax.set_xlabel(r"$x$ [m]", fontsize=18)
 plt.setp(ax.get_xticklabels(), fontsize=12)
-#ax.set_ylim((0,30))
+ax.set_ylim((0,30))
 ax.set_ylabel(r"$T_e$ [eV]", fontsize=18)
 plt.setp(ax.get_yticklabels(), fontsize=12)
 
-ax2 = ax.twinx()
-ax2.plot(xr, V0*phi[:,t0], 'b--', lw=2)
-ax2.plot(xr, V0*phi[:,t1], 'r--', lw=2)
-ax2.plot(xr, V0*phi[:,t2], 'g--', lw=2)
-ax2.plot(xr, V0*phi[:,t3], 'c--', lw=2)
+#ax2 = ax.twinx()
+#ax2.plot(xr, V0*phi[:,t0], 'b--', lw=2)
+#ax2.plot(xr, V0*phi[:,t1], 'r--', lw=2)
+#ax2.plot(xr, V0*phi[:,t2], 'g--', lw=2)
+#ax2.plot(xr, V0*phi[:,t3], 'c--', lw=2)
 #ax2.set_ylim((-120,120))
-ax2.set_ylabel(r"$\phi$ [V]", labelpad=-5, fontsize=18)
-plt.setp(ax2.get_yticklabels(), fontsize=12)
-plt.savefig('Te_phi_4spec_line.pdf')
+#ax2.set_ylabel(r"$\phi$ [V]", labelpad=-5, fontsize=18)
+#plt.setp(ax2.get_yticklabels(), fontsize=12)
+plt.savefig('Te_4spec_line.pdf')
 
 print("Plotting means...")
 
@@ -204,6 +210,7 @@ ax.set_ylabel(r"Density [m$^{-3}$]", fontsize=18)
 plt.setp(ax.get_yticklabels(), fontsize=12)
 plt.savefig('density_4spec_mean.pdf')
 
+# nm
 fig,ax = plt.subplots()
 ax.plot(xr, ne0*np.mean(nm,axis=1), 'g-', lw=2, label=r"$n^{\ast}$")
 ax.legend(fontsize=12)
@@ -225,11 +232,37 @@ plt.setp(ax.get_xticklabels(), fontsize=12)
 ax.set_ylim((0,5.5))
 ax.set_ylabel(r"$T_e$ [eV]", fontsize=18)
 plt.setp(ax.get_yticklabels(), fontsize=12)
+#
+#ax2 = ax.twinx()
+#ax2.plot(xr, V0*np.mean(phi,axis=1), 'r--', lw=2, label=r"$\phi$")
+#ax2.legend(fontsize=12,loc=1)
+##ax2.set_ylim((0,70))
+#ax2.set_ylabel(r"$\phi$ [V]", fontsize=18)
+#plt.setp(ax2.get_yticklabels(), fontsize=12)
+plt.savefig('Te_4spec_mean.pdf')
 
-ax2 = ax.twinx()
-ax2.plot(xr, V0*np.mean(phi,axis=1), 'r--', lw=2, label=r"$\phi$")
-ax2.legend(fontsize=12,loc=1)
-#ax2.set_ylim((0,70))
-ax2.set_ylabel(r"$\phi$ [V]", fontsize=18)
-plt.setp(ax2.get_yticklabels(), fontsize=12)
-plt.savefig('Te_phi_4spec_mean.pdf')
+# Write CSV File
+header = ['x', 'ne', 'ni', 'nm', 'nb', 'Te']
+data = []
+row = []
+for i in range(len(xr)):
+   x_temp = xr[i]
+   ne_temp = ne0*np.mean(ne,axis=1)[i]
+   ni_temp = ne0*np.mean(ni,axis=1)[i]
+   nm_temp = ne0*np.mean(nm,axis=1)[i]
+   nb_temp = ne0*np.mean(nb,axis=1)[i]
+   Te_temp = (2./3.)*11604.*np.mean(Te,axis=1)[i]
+   row.append(x_temp)
+   row.append(ne_temp)
+   row.append(ni_temp)
+   row.append(nm_temp)
+   row.append(nb_temp)
+   row.append(Te_temp)
+   data.append(row)
+   row = []
+
+f = open('GD_mean_results.csv', 'w')
+writer = csv.writer(f)
+writer.writerow(header)
+writer.writerows(data)
+f.close()
