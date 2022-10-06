Bug fix in Jacobian
Need to pass solve_poisson=True to jacobian evaluation b/c it seems
the poisson equation is evaluated twice in the residual call, once at
the current state as part of forming the residual and once at the
previous state as part of evaluating some qois.  This wasn't the
original design and makes the jacobian incorrect if the poisson solve
is not included (b/c the electric potential corresponds to the wrong
state).

chebSolver.py
@@ -1505,7 +1505,7 @@ class timeDomainCollocationSolver:
         while( not converged and (count < iter_max) ):
             #self.jacobianFD(self.U2, time, dt)
             #np.save("jacobian_FD.npy", self.jac)
-            self.jacobian(self.U2, time, dt, weak_bc)
+            self.jacobian(self.U2, time, dt, weak_bc, solve_poisson=True)
             #np.save("jacobian_AN.npy", self.jac)
 
             try: