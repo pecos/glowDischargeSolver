f56a50a9659202e00cfbf7f0941925b5a25f7628
Todd A. Oliver <oliver@oden.utexas.edu>
2022-10-21 16:56:48 -0500

    be more agressive with Newton solve on 6 species test

--- test6Species.sh
+++ test6Species.sh
@@ -25,7 +25,7 @@ newtFile="newton_6spec_CN_Np${Np}.npy"
 saveFile="newton_6spec_CN_Np${Np}_fullsoln.npy"
 
 baseCmd="$EXE --Np $Np --Nt $Nt --dt $dt --scenario $scenario --EinsteinForm --elasticCollisionActivation --backgroundSpecieActivation"
-newtCmd="$NEWTEXE --Np $Np --Nt $Nt1 --Nn 20 --scenario $scenario --tscheme CN --alpha0 0.1 --increaseFac 1.5 --EinsteinForm --elasticCollisionActivation --backgroundSpecieActivation"
+newtCmd="$NEWTEXE --Np $Np --Nt $Nt1 --Nn 20 --scenario $scenario --tscheme CN --alpha0 0.25 --increaseFac 2.0 --EinsteinForm --elasticCollisionActivation --backgroundSpecieActivation"
 saveCmd="$EXE --Np $Np --Nt $Nt1 --dt $dt --scenario $scenario --tscheme CN --EinsteinForm --elasticCollisionActivation --backgroundSpecieActivation"
 
 diffCmd="python3 ../diffSolns.py --reference ../reference_solns/Nominal_Np250_solution.npy --Np $Np --Nv 7"
