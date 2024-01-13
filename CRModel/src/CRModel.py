We changed the BE method by fixing the jacobian and calculating the jacobian only once at each time step. This lead to a performance speedup of 2.5. The jacobian doesn't change much so there was no need to evaluate it at each iteration in the Newton-Raphson loop.
NB: This feature is optional (see the input flag `freeze_jacobian` in
the `step` method of the `timeDomainCollocation` class).

CRModel/src/CRModel.py
@@ -16,7 +16,7 @@ import scipy.constants as spc
 # from scipy.optimize import fsolve,least_squares,root
 # from scipy.integrate import solve_ivp
 from scipy.optimize import approx_fprime
-from scipy.optimize._numdiff import approx_derivative,_eps_for_method
+# from scipy.optimize._numdiff import _eps_for_method,approx_derivative
 
 
 
@@ -217,20 +217,19 @@ class CollisionalRadiativeModel:
         """Evaluates the Jacobian for backward Euler time marching.
         """
 
-        xk = np.asarray(Uin, float)
-        f0 = self.rxnSourceTerm(xk)
-        return approx_derivative(self.rxnSourceTerm, xk, method='2-point', abs_step=1e-8,f0=f0)
+        # xk = np.asarray(Uin, float)
+        # f0 = self.rxnSourceTerm(xk)
+        # return approx_derivative(self.rxnSourceTerm, xk, method='2-point', abs_step=1e-8,f0=f0)
 
         # eps=1.0e-12
-        # omega_U = approx_fprime(Uin, self.rxnSourceTerm, epsilon=1e-8)
-        # return omega_U
+        return approx_fprime(Uin, self.rxnSourceTerm, epsilon=1e-8)
 
 
     def rxnSourceTermJac_2(self, Uin):
         """Evaluates the Jacobian for backward Euler time marching.
         """
 
-        epsilon=1e-8
+        epsilon=np.sqrt(np.finfo(float).eps)
 
         # user specifies an absolute step
         x0 = Uin
@@ -240,12 +239,14 @@ class CollisionalRadiativeModel:
 
         # cannot have a zero step. This might happen if x0 is very large
         # or small. In which case fall back to relative step.
-        
+                
         dx = ((x0 + h) - x0)
-        h = np.where(dx == 0,
-                     _eps_for_method(x0.dtype, x0.dtype, method) *
-                     sign_x0 * np.maximum(1.0, np.abs(x0)),
-                     h)
+        h = np.where(dx == 0, epsilon * sign_x0 * np.maximum(1.0, np.abs(x0)), h)
+
+        # h = np.where(dx == 0,
+        #              _eps_for_method(x0.dtype, x0.dtype, method) *
+        #              sign_x0 * np.maximum(1.0, np.abs(x0)),
+        #              h)
 
         omega_U = np.zeros((self.Ns+1,self.Ns+1),dtype=np.float64)
 
@@ -271,7 +272,7 @@ class CollisionalRadiativeModel:
         """
 
 
-        epsilon=1e-8
+        epsilon=np.sqrt(np.finfo(float).eps)
 
         # user specifies an absolute step
         x0 = Uin
@@ -282,10 +283,7 @@ class CollisionalRadiativeModel:
         # cannot have a zero step. This might happen if x0 is very large
         # or small. In which case fall back to relative step.
         dx = ((x0 + h) - x0)
-        h = np.where(dx == 0,
-                     _eps_for_method(x0.dtype, x0.dtype, method) *
-                     sign_x0 * np.maximum(1.0, np.abs(x0)),
-                     h)
+        h = np.where(dx == 0, epsilon * sign_x0 * np.maximum(1.0, np.abs(x0)), h)
     
 
 

chebSolver.py
@@ -17,12 +17,18 @@ sys.path.append(crmodel_dir)
 
 import numpy as np
 import numpy.polynomial.chebyshev as cheb
+# from scipy.sparse.linalg import cg
+# from scipy.linalg import solve
+# from scipy.sparse.linalg import gmres
+
 import time as cpu_time
 
 from Liu2014Properties import setLiu2014Properties
 from psaapPropertiesTestArm import setPsaapPropertiesTestArm
 from psaapPropertiesTestArmInterpTrans import setPsaapPropertiesTestArmInterpTrans
 from psaapProperties_6Species_Nominal import setPsaapProperties_6Species_Nominal
+
+from psaapProperties_6Species_1Torr_Simplified import setPsaapProperties_6Species_1Torr_Simplified
 
 from psaapProperties_CRModel_1Torr import setPsaapProperties_CRModel_1Torr
 from CRModel import CollisionalRadiativeModel
@@ -499,7 +505,8 @@ class timeDomainCollocationSolver:
         elif(scenario==6):
             setPsaapProperties_6Species_Sampling(gam, V0, VDC, self.params, Nr, iSample)
         elif(scenario==7):
-            setPsaapProperties_6Species_Sampling_250mTorr(gam, V0, VDC, self.params, Nr, iSample)
+            # setPsaapProperties_6Species_1Torr_Expanded(gam, V0, VDC, self.params, Nr, iSample)
+            setPsaapProperties_6Species_1Torr_Simplified(gam, V0, VDC, self.params, Nr, iSample)
         elif(scenario==8):
             setPsaapProperties_6Species_Sampling_500mTorr(gam, V0, VDC, self.params, Nr, iSample)
         elif(scenario==9):
@@ -1626,7 +1633,7 @@ class timeDomainCollocationSolver:
 
 
     def step(self, time, dt, iter_max=20,
-             rtol=1e-6, atol=1e-12, verbose=True, weak_bc=False):
+             rtol=1e-6, atol=1e-12, verbose=True, weak_bc=False, freeze_jacobian=False):
         """Take a single time step.
 
         Inputs
@@ -1641,20 +1648,26 @@ class timeDomainCollocationSolver:
         """
         r = self.residual(self.U2, time, dt, weak_bc)
 
+        if freeze_jacobian:
+            self.jacobian(self.U2, time, dt, weak_bc, solve_poisson=True)
+            jac_inv  = np.linalg.inv(self.jac)
+
         normr = normr0 = np.linalg.norm(r)
         count = 0
         converged = ((normr/normr0 < rtol) or (normr < atol))
         if (verbose):
             print("  {0:d}: ||res|| = {1:.6e}, ||res||/||res0|| = {2:.6e}".format(
                 count, normr, normr/normr0))
+
         while( not converged and (count < iter_max) ):
-            #self.jacobianFD(self.U2, time, dt)
-            #np.save("jacobian_FD.npy", self.jac)
-            self.jacobian(self.U2, time, dt, weak_bc, solve_poisson=True)
-            #np.save("jacobian_AN.npy", self.jac)
+            if (not freeze_jacobian):
+                self.jacobian(self.U2, time, dt, weak_bc, solve_poisson=True)
 
             try:
-                dU = np.linalg.solve(self.jac, -r)
+                if freeze_jacobian:
+                    dU = np.dot(jac_inv, -r)
+                else:
+                    dU = np.linalg.solve(self.jac, -r)
 
                 self.U2 += dU
 

post-processing/plot_results.py
@@ -10,11 +10,19 @@ import scipy.constants as spc
 isPlotLines = False
 isPlotMeans = True
 
-case1 = True; file1 = '../fullsoln.npy'; clr1 = 'b-'; label1 = "CR"
+# case1 = True; file1 = '../fullsoln.npy'; clr1 = 'b-'; label1 = "CR"
+case1 = True; file1 = '../Results/CR/CR_Np150_fullsoln.npy'; clr1 = 'b-'; label1 = "CR"
+
 
 # case1 = True; file1 = '../Results/CR/fullsoln.npy'; clr1 = 'b-'; label1 = "CR"
 # case2 = True; file2 = '../Results/6spec/fullsoln.npy'; clr2 = 'r-'; label2 = "6sp"
-case2 = False; file2 = '../Results/fullsoln.npy'; clr2 = 'r-'; label2 = "CR_2"
+# case2 = False; file2 = '../Results/fullsoln.npy'; clr2 = 'r-'; label2 = "CR_2"
+
+# case2 = True; file2 = '../Results/6spec/nominalCase_V100_P1torr_Np150/newton_6spec_CN_Np150_fullsoln.npy'; clr2 = 'b-'; label2 = "6spec"
+# case2 = True; file2 = '../Results/6spec/nominalCase_V100_P1torr_Np150_TimeMarching/newton_6spec_CN_Np150_fullsoln.npy'; clr2 = 'r-'; label2 = "6spec"
+case2 = True; file2 = '../Results/6spec/nominalCase_V100_P1torr_Np150_constDiff/newton_6spec_CN_Np150_fullsoln.npy'; clr2 = 'r-'; label2 = "6spec"
+# case2 = True; file2 = '../Results/6spec/nominalCase_V100_P1torr_Np150_dEps/newton_6spec_CN_Np150_fullsoln.npy'; clr2 = 'r-'; label2 = "6spec"
+
 
 # these values are required to "redimensionalize" the results
 # they must be consistent with the scenario input file
@@ -94,6 +102,10 @@ for i in range(6,15+1):
    print(i)
    n4p += D_reshaped[:,i,:] 
 
+# nm = D_reshaped[:,2,:]
+# nr = D_reshaped[:,3,:] 
+# n4p = D_reshaped[:,4,:] 
+
 # electron temp
 Te = (2./3.) * nee / ne  
 
@@ -109,7 +121,7 @@ if case2:
    D2 = np.transpose(D2)
 
    # Ns is the number of scpecies
-   Ns = 17 #  electrons + ions + nm + nr + n4p + nb 
+   Ns = 6 #  electrons + ions + nm + nr + n4p + nb 
 
    # pull solution out of D
    D_reshaped = np.reshape(D2,(Np, Ns+1, np.shape(D2)[1]),'F')
@@ -123,16 +135,16 @@ if case2:
    npop_2[:,0,:] = nb_2
    npop_2[:,1:,:] = D_reshaped[:,2:Ns-1,:]
 
-   # nm_2 = D_reshaped[:,2,:]
-   # nr_2 = D_reshaped[:,3,:] 
-   # n4p_2 = D_reshaped[:,4,:] 
+   nm_2 = D_reshaped[:,2,:]
+   nr_2 = D_reshaped[:,3,:] 
+   n4p_2 = D_reshaped[:,4,:] 
 
-   nm_2 = D_reshaped[:,2,:] + D_reshaped[:,4,:] 
-   nr_2 = D_reshaped[:,3,:] + D_reshaped[:,5,:] 
-   n4p_2 = np.zeros_like(nr_2)
-   for i in range(6,15+1):
-      print(i)
-      n4p_2 += D_reshaped[:,i,:] 
+   # nm_2 = D_reshaped[:,2,:] + D_reshaped[:,4,:] 
+   # nr_2 = D_reshaped[:,3,:] + D_reshaped[:,5,:] 
+   # n4p_2 = np.zeros_like(nr_2)
+   # for i in range(6,15+1):
+   #    print(i)
+   #    n4p_2 += D_reshaped[:,i,:] 
    
    # electron temp
    Te_2 = (2./3.) * nee_2 / ne_2  